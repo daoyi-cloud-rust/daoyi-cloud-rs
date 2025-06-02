ecca2ff3e34351cc2d1d737b6667a4a388498f42
Gemiman <913573586@qq.com>
2025-06-02 09:51:03 +0800

    优化部门服务错误处理，使用统一业务错误码替代直接HTTP状态码返回

--- daoyi-cloud-service/src/service/system/system_dept_service.rs
+++ daoyi-cloud-service/src/service/system/system_dept_service.rs
@@ -3,10 +3,8 @@ use daoyi_cloud_entities::entities::system::prelude::SystemDept;
 use daoyi_cloud_entities::entities::system::system_dept;
 use daoyi_cloud_models::models::biz_error;
 use daoyi_cloud_models::models::common_result::AppResult;
-use daoyi_cloud_models::models::error::AppError;
 use daoyi_cloud_models::models::system::dept_save_req_vo::DeptSaveReqVo;
 use daoyi_cloud_models::models::system::system_oauth2_access_token::OAuth2AccessTokenCheckRespDTO;
-use salvo::http::{StatusCode, StatusError};
 use sea_orm::ColumnTrait;
 use sea_orm::QueryFilter;
 use sea_orm::{ActiveModelTrait, EntityTrait, Set};
@@ -64,22 +62,14 @@ async fn validate_parent_dept(id: &Option<i64>, parent_id: &Option<i64>) -> AppR
     }
     // 1. 不能设置自己为父部门
     if id.is_some() && id.unwrap() == parent_id.unwrap() {
-        return Err(AppError::HttpStatus(
-            StatusError::from_code(StatusCode::BAD_REQUEST)
-                .unwrap()
-                .brief("不能设置自己为父部门"),
-        ));
+        return biz_error::DEPT_PARENT_ERROR.to_app_result();
     }
     // 2. 父部门不存在
     let option = SystemDept::find_by_id(parent_id.unwrap())
         .one(db::pool())
         .await?;
     if option.is_none() {
-        return Err(AppError::HttpStatus(
-            StatusError::from_code(StatusCode::BAD_REQUEST)
-                .unwrap()
-                .brief("父部门不存在"),
-        ));
+        return biz_error::DEPT_PARENT_NOT_EXITS.to_app_result();
     }
     // 3. 递归校验父部门，如果父部门是自己的子部门，则报错，避免形成环路
     if id.is_none() {
@@ -90,11 +80,7 @@ async fn validate_parent_dept(id: &Option<i64>, parent_id: &Option<i64>) -> AppR
         // 3.1 校验环路
         let parent_id = parent.parent_id;
         if parent_id == id.unwrap() {
-            return Err(AppError::HttpStatus(
-                StatusError::from_code(StatusCode::BAD_REQUEST)
-                    .unwrap()
-                    .brief("不能设置自己的子部门为父部门"),
-            ));
+            return biz_error::DEPT_PARENT_IS_CHILD.to_app_result();
         }
         // 3.2 继续递归下一级父部门
         if parent_id == system_dept::PARENT_ID_ROOT {
