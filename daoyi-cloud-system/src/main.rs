6f1e4436a7592064e60f4bcfacdfe3c3a9dc3fc6
Gemiman <913573586@qq.com>
2025-06-05 10:39:14 +0800

    添加axum和tokio依赖，实现基础HTTP服务

--- daoyi-cloud-system/src/main.rs
+++ daoyi-cloud-system/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use axum::{Router, routing};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+    let router = Router::new().route("/", routing::get(async || "Hello, world!"));
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Listening on http://{}", listener.local_addr().unwrap());
+    axum::serve(listener, router).await.unwrap();
 }
