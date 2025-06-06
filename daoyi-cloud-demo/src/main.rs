3163465ee9e02ebfa2c42727ae5b145cc4993634
Gemiman <913573586@qq.com>
2025-06-06 09:34:31 +0800

    添加环境变量示例文件和实体模块，更新workspace成员配置

---
+++ daoyi-cloud-demo/src/main.rs
@@ -0,0 +1,26 @@
+use axum::{Router, debug_handler, routing};
+use daoyi_cloud_config::config;
+use daoyi_cloud_config::config::database;
+use daoyi_cloud_logger::logger;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // 设置环境变量（只在未设置时设置）
+    unsafe {
+        std::env::set_var("APP_ROOT", env!("CARGO_MANIFEST_DIR"));
+    }
+    logger::init(Some("debug"));
+    database::init().await?;
+    let router = Router::new().route("/", routing::get(hello_world));
+    let port = config::get().server().port();
+    let listener = TcpListener::bind(format!("0.0.0.0:{port}")).await?;
+    logger::debug!("Listening on {}://{}", "http", listener.local_addr()?);
+    axum::serve(listener, router).await?;
+    Ok(())
+}
+
+#[debug_handler]
+async fn hello_world() -> String {
+    format!("Hello, {} ~", config::get().app_name())
+}
