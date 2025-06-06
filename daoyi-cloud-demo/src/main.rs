ce314e273bfac053ef4c24e54d49587b0a901099
Gemiman <913573586@qq.com>
2025-06-06 15:43:51 +0800

    重构应用启动流程，迁移日志模块至server模块，优化用户查询条件并简化数据库访问

--- daoyi-cloud-demo/src/main.rs
+++ daoyi-cloud-demo/src/main.rs
@@ -1,13 +1,13 @@
+use axum::extract::State;
 use axum::response::IntoResponse;
 use axum::{Router, debug_handler, routing};
 use daoyi_cloud_config::config;
-use daoyi_cloud_config::config::database;
 use daoyi_cloud_entity::entity::demo::prelude::*;
 use daoyi_cloud_entity::entity::demo::system_users;
-use daoyi_cloud_logger::logger;
+use daoyi_cloud_server::app;
+use daoyi_cloud_server::app::AppState;
 use sea_orm::Condition;
 use sea_orm::prelude::*;
-use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -15,33 +15,26 @@ async fn main() -> anyhow::Result<()> {
     unsafe {
         std::env::set_var("APP_ROOT", env!("CARGO_MANIFEST_DIR"));
     }
-    logger::init(Some("debug"));
-    database::init().await?;
     let router = Router::new()
         .route("/", routing::get(hello_world))
         .route("/users", routing::get(get_users));
-    let port = config::get().server().port();
-    let listener = TcpListener::bind(format!("0.0.0.0:{port}")).await?;
-    logger::info!("Listening on {}://{}", "http", listener.local_addr()?);
-    axum::serve(listener, router).await?;
-    Ok(())
+    app::run(router).await
 }
 
 #[debug_handler]
-async fn get_users() -> impl IntoResponse {
+async fn get_users(State(AppState { db }): State<AppState>) -> impl IntoResponse {
     let users = SystemUsers::find()
-        .filter(system_users::Column::Deleted.eq(true))
+        .filter(system_users::Column::Deleted.eq(false))
         .filter(
             Condition::all()
-                .add(system_users::Column::Sex.eq(1))
-                .add(system_users::Column::TenantId.gt(18))
-                .add(
-                    Condition::any()
-                        .add(system_users::Column::Username.contains("张"))
-                        .add(system_users::Column::Nickname.contains("李")),
-                ),
+                // .add(system_users::Column::Sex.eq(1))
+                .add(system_users::Column::TenantId.eq(1)), // .add(
+                                                            //     Condition::any()
+                                                            //         .add(system_users::Column::Username.contains("张"))
+                                                            //         .add(system_users::Column::Nickname.contains("李")),
+                                                            // ),
         )
-        .all(database::pool1())
+        .all(db)
         .await
         .unwrap();
     axum::Json(users)
