7475546bdfc0f833d913de5f4f53d1b2a534a270
Gemiman <913573586@qq.com>
2025-06-25 00:26:17 +0800

    新增基础实体特征模块，实现系统实体公共字段统一管理，为部门、字典、错误码等系统模块添加创建者、更新者和租户ID设置方法，重构实体模型实现BaseEntityTrait特征，提升代码复用性

--- lib/daoyi-cloud-entity/src/entity/mod.rs
+++ lib/daoyi-cloud-entity/src/entity/mod.rs
@@ -1,2 +1,8 @@
 pub mod demo;
 pub mod system;
+
+pub trait BaseEntityTrait {
+    fn set_creator(&mut self, u: String);
+    fn set_updater(&mut self, u: String);
+    fn set_tenant_id(&mut self, _t: i64) {}
+}
