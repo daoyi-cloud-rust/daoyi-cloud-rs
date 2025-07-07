5d45d9aae3dfe589a47813719758d9883e87b3e0
Gemiman <913573586@qq.com>
2025-07-07 18:10:49 +0800

    新增密码加密接口，实现GET方式密码加密功能，添加base64密码编码工具方法，完善认证模块路由配置，支持通过路径参数获取加密结果

--- lib/daoyi-cloud-api/src/api/admin_api/system/auth/mod.rs
+++ lib/daoyi-cloud-api/src/api/admin_api/system/auth/mod.rs
@@ -2,10 +2,19 @@ use crate::service::system::auth::admin_auth_service::AdminAuthService;
 use axum::debug_handler;
 use axum::extract::State;
 use daoyi_cloud_common::error::ApiResult;
+use daoyi_cloud_common::models::api_extract::path::Path;
 use daoyi_cloud_common::models::api_extract::valid::ValidJson;
 use daoyi_cloud_common::models::app_server::AppState;
 use daoyi_cloud_common::response::ApiResponse;
+use daoyi_cloud_common::utils::base64_util;
 use daoyi_cloud_entity::vo::auth::{AuthLoginReqVo, AuthLoginRespVo, AuthPermissionInfoRespVo};
+
+/// 加密密码
+#[debug_handler]
+pub async fn encode_password(Path(passwd): Path<String>) -> ApiResult<String> {
+    let res = base64_util::encode_password(&passwd).await?;
+    ApiResponse::okk(Some(res))
+}
 
 /// 使用账号密码登录
 #[debug_handler]
